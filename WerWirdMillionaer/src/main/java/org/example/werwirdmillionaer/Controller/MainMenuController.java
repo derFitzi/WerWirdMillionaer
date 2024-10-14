776c548427c170b9b68dee17e463c5897fde203a
Marc Embacher <mqrcxxz@gmail.com>
2024-10-14 13:15:21 +0200

    MainMenu

---
+++ WerWirdMillionaer/src/main/java/org/example/werwirdmillionaer/Controller/MainMenuController.java
@@ -0,0 +1,19 @@
+package org.example.werwirdmillionaer.Controller;
+
+public class MainMenuController {
+    public void startGame() {
+        System.out.println("Game started!");
+    }
+
+    public void showHighscore() {
+        System.out.println("Highscore shown!");
+    }
+
+    public void showSettings() {
+        System.out.println("Settings shown!");
+    }
+
+    public void exitGame() {
+        System.out.println("Game exited!");
+    }
+}
